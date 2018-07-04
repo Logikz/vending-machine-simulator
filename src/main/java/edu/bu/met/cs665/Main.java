5a695c005458e142b02321274636e2c89663b265
Logikz <martianman@cox.net>
2018-07-04 12:03:12 -0400

    Initial commit updating project to be buildable and compilable

--- src/main/java/edu/bu/met/cs665/Main.java
+++ src/main/java/edu/bu/met/cs665/Main.java
@@ -1,20 +1,9 @@
 package edu.bu.met.cs665;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import edu.bu.met.cs665.example1.Person;
-
 public class Main {
 
 	public static void main(String[] args) {
-
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-		LocalDate xmas = LocalDate.parse("22/12/1994", formatter);
-		Person student = new Person("John", "Doe", xmas);
-
-		System.out.println(student.calculateAge());
+		System.out.println("Hello world");
 	}
 
 }
